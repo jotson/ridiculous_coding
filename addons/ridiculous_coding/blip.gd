7cb98aaf8a35460b0375b3c1dbb52a10c2c4bacd
jitspoe <jitspoe@yahoo.com>
2021-05-23 23:02:30 -0400

    Pitch increases with every key press and decreases over time to make things more exciting.

--- addons/ridiculous_coding/blip.gd
+++ addons/ridiculous_coding/blip.gd
@@ -3,8 +3,10 @@ extends Node2D
 
 var destroy = false
 var last_key = ""
+var pitch_increase = 0
 
 func _ready():
+	$AudioStreamPlayer.pitch_scale = 1.0 + pitch_increase * 0.01
 	$AudioStreamPlayer.play()
 	$AnimatedSprite.frame = 0
 	$AnimatedSprite.play("default")
