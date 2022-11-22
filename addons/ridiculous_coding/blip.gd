e67a8b5901b1fabfc4f73cae69e27afe11c571dd
Jeferson Leite Borges <jeferson_leite_borges@epam.com>
2022-11-22 18:01:08 +0100

    Updated to Godot4

--- addons/ridiculous_coding/blip.gd
+++ addons/ridiculous_coding/blip.gd
@@ -1,25 +1,35 @@
-tool
+@tool
 extends Node2D
 
-var destroy = false
-var last_key = ""
-var pitch_increase = 0
-var sound = true
-var blips = true
+var destroy: bool = false
+var last_key: String = ""
+var pitch_increase: float = 0.0
+var sound: bool = true
+var blips: bool = true
+
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animated_player: AnimationPlayer = $AnimationPlayer
+@onready var gpu_particle_2d: GPUParticles2D = $GPUParticles2D
+@onready var timer: Timer = $Timer
+@onready var label: Label = $Label
 
 
 func _ready():
 	if sound:
-		$AudioStreamPlayer.pitch_scale = 1.0 + pitch_increase * 0.01
-		$AudioStreamPlayer.play()
+		audio_stream_player.pitch_scale = 1.0 + pitch_increase * 0.01
+		audio_stream_player.play()
+	
 	if blips:
-		$AnimatedSprite.frame = 0
-		$AnimatedSprite.play("default")
-		$AnimationPlayer.play("default")
-		$Particles2D.emitting = true
-	$Timer.start()
-	$Label.text = last_key
-	$Label.modulate = Color(rand_range(0,2), rand_range(0,2), rand_range(0,2))
+		animated_sprite_2d.frame = 0
+		animated_sprite_2d.play("default")
+		animated_player.play("default")
+		gpu_particle_2d.emitting = true
+	
+	timer.start()
+	label.text = last_key
+	label.modulate = Color(randf_range(0,2), randf_range(0,2), randf_range(0,2))
+
 
 func _on_Timer_timeout():
 	if destroy:
--- addons/ridiculous_coding/plugin.gd
+++ addons/ridiculous_coding/plugin.gd
@@ -1,32 +1,34 @@
-tool
+@tool
 extends EditorPlugin
-
-var shake = 0.0
-var shake_intensity = 0.0
-var timer = 0.0
-var last_key = ""
-var pitch_increase := 0.0
-var editors = {}
-const PITCH_DECREMENT := 2.0
-
-const Boom = preload("boom.tscn")
-const Blip = preload("blip.tscn")
-const Newline = preload("newline.tscn")
-
-const Dock = preload("res://addons/ridiculous_coding/dock.tscn")
-var dock
 
 signal typing
 
+# Scenes preloaded
+const Boom: PackedScene = preload("res://addons/ridiculous_coding/boom.tscn")
+const Blip: PackedScene = preload("res://addons/ridiculous_coding/blip.tscn")
+const Newline: PackedScene = preload("res://addons/ridiculous_coding/newline.tscn")
+const Dock: PackedScene = preload("res://addons/ridiculous_coding/dock.tscn")
+
+# Inner Variables
+const PITCH_DECREMENT := 2.0
+
+var shake: float = 0.0
+var shake_intensity:float  = 0.0
+var timer: float = 0.0
+var last_key: String = ""
+var pitch_increase: float = 0.0
+var editors = {}
+var dock
+
 
 func _enter_tree():
-	var editor = get_editor_interface()
-	var script_editor = editor.get_script_editor()
-	script_editor.connect("editor_script_changed", self, "editor_script_changed")
+	var editor: EditorInterface = get_editor_interface()
+	var script_editor: ScriptEditor = editor.get_script_editor()
+	script_editor.editor_script_changed.connect(editor_script_changed)
 
 	# Add the main panel
-	dock = Dock.instance()
-	connect("typing", dock, "_on_typing")
+	dock = Dock.instantiate()
+	typing.connect(Callable(dock,"_on_typing"))
 	add_control_to_dock(DOCK_SLOT_RIGHT_BL, dock)
 
 
@@ -34,7 +36,7 @@ func _exit_tree():
 	if dock:
 		remove_control_from_docks(dock)
 		dock.free()
-		
+
 
 func get_all_text_editors(parent : Node):
 	for child in parent.get_children():
@@ -42,27 +44,30 @@ func get_all_text_editors(parent : Node):
 			get_all_text_editors(child)
 			
 		if child is TextEdit:
-			editors[child] = { "text": child.text, "line": child.cursor_get_line() }
+			editors[child] = { 
+				"text": child.text, 
+				"line": child.get_caret_line() 
+			}
 			
-			if child.is_connected("cursor_changed", self, "cursor_changed"):
-				child.disconnect("cursor_changed", self, "cursor_changed")
-			child.connect("cursor_changed", self, "cursor_changed", [child])
-				
-			if child.is_connected("text_changed", self, "text_changed"):
-				child.disconnect("text_changed", self, "text_changed")
-			child.connect("text_changed", self, "text_changed", [child])
-
-			if child.is_connected("gui_input", self, "gui_input"):
-				child.disconnect("gui_input", self, "gui_input")
-			child.connect("gui_input", self, "gui_input")
+			if child.caret_changed.is_connected(caret_changed):
+				child.caret_changed.disconnect(caret_changed)
+			child.caret_changed.connect(caret_changed.bind(child))
+			
+			if child.text_changed.is_connected(text_changed):
+				child.text_changed.disconnect(text_changed)
+			child.text_changed.connect(text_changed.bind(child))
+			
+			if child.gui_input.is_connected(gui_input):
+				child.gui_input.disconnect(gui_input)
+			child.gui_input.connect(gui_input)
 
 
 func gui_input(event):
 	# Get last key typed
 	if event is InputEventKey and event.pressed:
 		event = event as InputEventKey
-		last_key = OS.get_scancode_string(event.get_scancode_with_modifiers())
-		
+		last_key = OS.get_keycode_string(event.get_keycode_with_modifiers())
+
 
 func editor_script_changed(script):
 	var editor = get_editor_interface()
@@ -77,24 +82,24 @@ func _process(delta):
 	
 	if shake > 0:
 		shake -= delta
-		editor.get_base_control().rect_position = Vector2(rand_range(-shake_intensity,shake_intensity), rand_range(-shake_intensity,shake_intensity))
+		editor.get_base_control().position = Vector2(randf_range(-shake_intensity,shake_intensity), randf_range(-shake_intensity,shake_intensity))
 	else:
-		editor.get_base_control().rect_position = Vector2.ZERO
-
+		editor.get_base_control().position = Vector2.ZERO
+	
 	timer += delta
 	if (pitch_increase > 0.0):
 		pitch_increase -= delta * PITCH_DECREMENT
 
 
-func shake(duration, intensity):
+func shake_screen(duration, intensity):
 	if shake > 0:
 		return
 		
 	shake = duration
 	shake_intensity = intensity
-	
-	
-func cursor_changed(textedit):
+
+
+func caret_changed(textedit):
 	var editor = get_editor_interface()
 	
 	if not editors.has(textedit):
@@ -103,12 +108,12 @@ func cursor_changed(textedit):
 		editors.clear()
 		get_all_text_editors(editor.get_script_editor())
 		
-	editors[textedit]["line"] = textedit.cursor_get_line()
+	editors[textedit]["line"] = textedit.get_caret_line()
 
 
 func text_changed(textedit : TextEdit):
-	var editor = get_editor_interface()
-	var settings = editor.get_editor_settings()
+	var editor: EditorInterface = get_editor_interface()
+	var settings: EditorSettings = editor.get_editor_settings()
 	
 	if not editors.has(textedit):
 		# For some reason the editor instances all change
@@ -117,22 +122,17 @@ func text_changed(textedit : TextEdit):
 		get_all_text_editors(editor.get_script_editor())
 	
 	# Get line and character count
-	var line = textedit.cursor_get_line()
-	var column = textedit.cursor_get_column()
+	var line = textedit.get_caret_line()
+	var column = textedit.get_caret_column()
 	
 	# Compensate for code folding
-	var folding_adjustment = 0
-	for i in range(textedit.get_line_count()):
-		if i > line:
-			break
-		if textedit.is_line_hidden(i):
-			folding_adjustment += 1
-
+	var folding_adjustment = line - textedit.get_visible_line_count_in_range(0, line-1)
+	
 	# Compensate for tab size
-	var tab_size = settings.get_setting("text_editor/indent/size")
+	var tab_size = settings.get_setting("text_editor/behavior/indent/size")
 	var line_text = textedit.get_line(line).substr(0,column)
 	column += line_text.count("\t") * (tab_size - 1)
-
+	
 	# Compensate for scroll
 	var vscroll = textedit.scroll_vertical
 	var hscroll = textedit.scroll_horizontal
@@ -140,34 +140,34 @@ func text_changed(textedit : TextEdit):
 	# When you are scrolled to the bottom of a file
 	# and you delete some lines from the bottom using Ctrl+X
 	# then the vscroll can go down without changing the visible
-	# scroll position. That throws off the calculation because
+	# scroll position. That throws unchecked the calculation because
 	# we're calculating the position from the lower position but
 	# visually the position hasn't moved. By setting vscroll
 	# to the new actual position, the editor moves the visible
-	# lines to remove the gap. It changes the editor behavior
+	# lines to remove_at the gap. It changes the editor behavior
 	# slightly for a better result.
 	textedit.scroll_vertical = vscroll
 	
 	# Compensate for line spacing
-	var line_spacing = settings.get_setting("text_editor/theme/line_spacing")
+	var line_spacing = settings.get_setting("text_editor/appearance/whitespace/line_spacing")
 	
 	# Load editor font
-	var font : DynamicFont = DynamicFont.new()
-	font.font_data = load(settings.get_setting("interface/editor/code_font"))
-	font.size = settings.get_setting("interface/editor/code_font_size")
+	var font : FontFile = FontFile.new()
+	font.load_dynamic_font(settings.get_setting("interface/editor/code_font"))
+	font.fixed_size = settings.get_setting("interface/editor/code_font_size")
 	var fontsize = font.get_string_size(" ")
 	
 	# Compensate for editor scaling
 	var scale = editor.get_editor_scale()
-
+	
 	# Compute gutter width in characters
 	var line_count = textedit.get_line_count()
 	var gutter = str(line_count).length() + 6
 	
 	# Compute caret position
 	var pos = Vector2()
-	pos.x = (gutter + column) * fontsize.x * scale - hscroll
-	pos.y = (line - folding_adjustment - vscroll) * (fontsize.y + line_spacing - 2) + 16
+	pos.x = ((gutter + column) * fontsize.x * scale - hscroll) + 60
+	pos.y = (line - folding_adjustment - vscroll) * (fontsize.y + line_spacing - 2) + 10
 	pos.y *= scale
 	
 	emit_signal("typing")
@@ -179,7 +179,7 @@ func text_changed(textedit : TextEdit):
 			
 			if dock.explosions:
 				# Draw the thing
-				var thing = Boom.instance()
+				var thing = Boom.instantiate()
 				thing.position = pos
 				thing.destroy = true
 				if dock.chars: thing.last_key = last_key
@@ -188,14 +188,14 @@ func text_changed(textedit : TextEdit):
 				
 				if dock.shake:
 					# Shake
-					shake(0.2, 10)
-
+					shake_screen(0.2, 10)
+		
 		# Typing
 		if timer > 0.02 and len(textedit.text) >= len(editors[textedit]["text"]):
 			timer = 0.0
-
+			
 			# Draw the thing
-			var thing = Blip.instance()
+			var thing = Blip.instantiate()
 			thing.pitch_increase = pitch_increase
 			pitch_increase += 1.0
 			thing.position = pos
@@ -207,12 +207,12 @@ func text_changed(textedit : TextEdit):
 			
 			if dock.shake:
 				# Shake
-				shake(0.05, 5)
+				shake_screen(0.05, 5)
 			
 		# Newline
-		if textedit.cursor_get_line() != editors[textedit]["line"]:
+		if textedit.get_caret_line() != editors[textedit]["line"]:
 			# Draw the thing
-			var thing = Newline.instance()
+			var thing = Newline.instantiate()
 			thing.position = pos
 			thing.destroy = true
 			thing.blips = dock.blips
@@ -220,8 +220,7 @@ func text_changed(textedit : TextEdit):
 			
 			if dock.shake:
 				# Shake
-				shake(0.05, 5)
-
-	editors[textedit]["text"] = textedit.text
-	editors[textedit]["line"] = textedit.cursor_get_line()
+				shake_screen(0.05, 5)
 	
+	editors[textedit]["text"] = textedit.text
+	editors[textedit]["line"] = textedit.get_caret_line()
